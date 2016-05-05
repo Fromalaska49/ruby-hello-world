1460ecd51e17f70b2cf6766f4359eef5df6eda57
Andrew Hutton <fromalaska49@gmail.com>
2016-05-05 10:27:32 -0500

    Hello World

---
+++ hello-world.rb
@@ -0,0 +1,18 @@
+<% page_title = "It's working!" %>
+<% salutation = "Hello world!" %>
+
+<html>
+	<head>
+		<title>
+			<%= page_title %>
+		</title>
+	</head>
+	<body>
+		<div>
+			<%= salutation %>
+		</div>
+		<div>
+			This is an example of how ERB fills out a template.
+		</div>
+	</body>
+</html>
