5727bfeeea1031912bbf4feef5577fb2e9ef1200
Andrew Hutton <fromalaska49@gmail.com>
2016-05-05 11:32:04 -0500

    Adding Index Page

---
+++ index.rb
@@ -0,0 +1,19 @@
+<%
+newvar = "Hello world!"
+%>
+
+<html>
+	<head>
+		<title>
+			Home
+		</title>
+	</head>
+	<body>
+		<h1>
+			<%= newvar %>
+		</h1>
+		<div>
+			<%= "It's working" %>
+		</div>
+	</body>
+</html>
